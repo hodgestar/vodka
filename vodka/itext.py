a36cc3cc427b523c7e95bf58b56f3f4f5ca4006f
Simon Cross <hodgestar@gmail.com>
2011-12-20 14:07:07 +0200

    Implement itext support with tests.

--- vodka/itext.py
+++ vodka/itext.py
@@ -26,14 +26,61 @@ Example itext section::
 
 """
 
+import re
 
-class XFormsIText(object):
+
+class IText(object):
     """
     Holds itext data.
     """
 
+    JR_ITEXT = re.compile(r"jr:itext\(['\"](?P<id>.*)['\"]\)")
+
     def __init__(self, elem):
-        self.elem = elem
+        self.translations = {}
+        for trans_elem in elem.findall('translation'):
+            lang = trans_elem.attrib['lang']
+            lookup = {}
+            for text in trans_elem.findall('text'):
+                xid = text.attrib['id']
+                value = text.find('value').text
+                lookup[xid] = value
+            self.translations[lang] = lookup
+
+    def translator(self, lang):
+        """Return a function that translates references.
+
+        Equivalent to `lambda ref: self.translation(lang, ref)`.
+        """
+        if lang not in self.translations:
+            raise UnknownLanguage("Unknown language: %r" % lang)
+
+        def translation(ref):
+            return self.translation(lang, ref)
+        return translation
 
     def translation(self, lang, ref):
-        pass
+        """Return a translation string for the given language and reference."""
+        match = self.JR_ITEXT.match(ref)
+        if not match:
+            raise RefParseError("Can't handle ref: %r" % ref)
+        lookup = self.translations.get(lang)
+        if lookup is None:
+            raise UnknownLanguage("Unknown language: %r" % lang)
+        xid = match.group('id')
+        if xid not in lookup:
+            raise UnknownReference("Unknown reference (language %r): %r"
+                                   % (lang, ref))
+        return lookup[xid]
+
+
+class UnknownLanguage(Exception):
+    """Raised when a language isn't present in the translations."""
+
+
+class UnknownReference(Exception):
+    """Raised when a reference isn't present in the translations."""
+
+
+class RefParseError(Exception):
+    """Raised when a refernece attribute cannot be interpretted."""
--- vodka/tests/test_itext.py
+++ vodka/tests/test_itext.py
@@ -1,18 +1,66 @@
 """Test for vodka.itext."""
 
 import unittest
+from xml.etree import ElementTree
+
+from vodka.itext import IText, UnknownLanguage, UnknownReference, RefParseError
+
+
+EXAMPLE1 = """
+<itext>
+  <translation lang="eng">
+    <text id="/data/Name:label">
+      <value>Enter your full name</value>
+    </text>
+    <text id="/data/Name:hint">
+      <value/>
+    </text>
+  </translation>
+  <translation lang="afr">
+    <text id="/data/Name:label">
+      <value>Gee jou volle naam</value>
+    </text>
+    <text id="/data/Name:hint">
+      <value/>
+    </text>
+  </translation>
+</itext>
+"""
 
 
 class TestXFormsIText(unittest.TestCase):
 
+    def mk_itext(self, xml):
+        elem = ElementTree.fromstring(xml)
+        return IText(elem)
+
     def test_simple_translation(self):
-        raise ValueError()
+        itext = self.mk_itext(EXAMPLE1)
+        eng = itext.translator("eng")
+        self.assertEqual(eng("jr:itext('/data/Name:label')"),
+                         "Enter your full name")
+        self.assertEqual(eng("jr:itext('/data/Name:hint')"),
+                         None)
+
+    def test_other_language(self):
+        itext = self.mk_itext(EXAMPLE1)
+        afr = itext.translator("afr")
+        self.assertEqual(afr("jr:itext('/data/Name:label')"),
+                         "Gee jou volle naam")
+        self.assertEqual(afr("jr:itext('/data/Name:hint')"),
+                         None)
 
     def test_missing_lang(self):
-        raise ValueError()
+        itext = self.mk_itext(EXAMPLE1)
+        self.assertRaises(UnknownLanguage, itext.translator, "zoo")
+        self.assertRaises(UnknownLanguage, itext.translation, "zoo",
+                          "jr:itext('/data/Name:hint')")
 
     def test_missing_ref(self):
-        raise ValueError()
+        itext = self.mk_itext(EXAMPLE1)
+        self.assertRaises(UnknownReference, itext.translation, "eng",
+                          "jr:itext('/data/Name:unknown')")
 
     def test_unsupported_ref(self):
-        raise ValueError()
+        itext = self.mk_itext(EXAMPLE1)
+        self.assertRaises(RefParseError, itext.translation, "eng", "bad:ref")
