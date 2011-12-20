180ce4d05ce1fa82f3e5ec8ef8f677c7c63747f0
Simon Cross <hodgestar@gmail.com>
2011-12-20 13:11:16 +0200

    Skeleton with failing tests for itext.

--- vodka/itext.py
+++ vodka/itext.py
@@ -1,5 +1,29 @@
+# -*- test-case-name: vodka.tests.test_itext -*-
+
 """
 XForms itext implementation.
+
+Example itext section::
+
+    <itext>
+      <translation lang="eng">
+        <text id="/data/Name:label">
+          <value>Enter your full name</value>
+        </text>
+        <text id="/data/Name:hint">
+          <value/>
+        </text>
+      </translation>
+      <translation lang="afr">
+        <text id="/data/Name:label">
+          <value>Gee jou volle naam</value>
+        </text>
+        <text id="/data/Name:hint">
+          <value/>
+        </text>
+      </translation>
+    </itext>
+
 """
 
 
@@ -7,3 +31,9 @@ class XFormsIText(object):
     """
     Holds itext data.
     """
+
+    def __init__(self, elem):
+        self.elem = elem
+
+    def translation(self, lang, ref):
+        pass
---
+++ vodka/tests/test_itext.py
@@ -0,0 +1,18 @@
+"""Test for vodka.itext."""
+
+import unittest
+
+
+class TestXFormsIText(unittest.TestCase):
+
+    def test_simple_translation(self):
+        raise ValueError()
+
+    def test_missing_lang(self):
+        raise ValueError()
+
+    def test_missing_ref(self):
+        raise ValueError()
+
+    def test_unsupported_ref(self):
+        raise ValueError()
