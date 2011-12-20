93ed972040a1a7b287fb6cfd63d8aeef5a9f38b5
Simon Cross <hodgestar@gmail.com>
2011-12-20 15:50:12 +0200

    Some progress towards OdkForm.

---
+++ vodka/tests/test_xforms.py
@@ -0,0 +1,55 @@
+"""Tests for vodka.xforms."""
+
+import unittest
+from StringIO import StringIO
+
+from vodka.xforms import OdkForm
+
+EXAMPLE1 = """
+<h:html xmlns="http://www.w3.org/2002/xforms"
+        xmlns:h="http://www.w3.org/1999/xhtml"
+        xmlns:ev="http://www.w3.org/2001/xml-events"
+        xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+        xmlns:jr="http://openrosa.org/javarosa">
+  <h:head>
+    <h:title>Untitled Form</h:title>
+    <model>
+      <instance>
+        <data id="build_Untitled-Form_1323432952">
+          <Name>
+            Joe Blogs
+          </Name>
+        </data>
+      </instance>
+      <itext>
+        <translation lang="eng">
+          <text id="/data/Name:label">
+            <value>Enter your full name</value>
+          </text>
+          <text id="/data/Name:hint">
+            <value/>
+          </text>
+        </translation>
+      </itext>
+      <bind nodeset="/data/Name" type="string" required="true()"/>
+    </model>
+  </h:head>
+  <h:body>
+    <input ref="/data/Name">
+      <label ref="jr:itext('/data/Name:label')"/>
+      <hint ref="jr:itext('/data/Name:hint')"/>
+    </input>
+  </h:body>
+</h:html>
+"""
+
+
+class TestOdkForm(unittest.TestCase):
+
+    def test_create_form_from_string(self):
+        odk = OdkForm(EXAMPLE1)
+        raise ValueError()
+
+    def test_create_form_from_fileobj(self):
+        fileobj = StringIO(EXAMPLE1)
+        odk = OdkForm(fileobj)
--- vodka/xforms.py
+++ vodka/xforms.py
@@ -1,8 +1,15 @@
+# -*- test-case-name: vodka.tests.test_xforms -*-
+
 """
 XForms document types.
 
 For now, just ODK forms.
 """
+
+from xml.etree.ElementTree import fromstring, parse
+
+from vodka.xtypes import XFormsModel
+from vodka.xinputs import XFormsInput
 
 
 class OdkForm(object):
@@ -11,3 +18,15 @@ class OdkForm(object):
 
     Currently specific to the output of the online ODK builder.
     """
+
+    def __init__(self, source):
+        if isinstance(source, basestring):
+            doc = fromstring(source)
+        elif hasattr(source, 'read') and callable(source.read):
+            doc = parse(source)
+        else:
+            # assume it's an Element instance
+            doc = source
+        self.model = XFormsModel(doc.find('model'))
+        self.inputs = [XFormsInput.from_element(elem)
+                       for elem in doc.find('body')]
