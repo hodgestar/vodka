b1a1ba0ed553e7fa889cca65691bb115f8b303c4
Jeremy Thurgood <jeremy@praekelt.com>
2011-12-20 15:24:39 +0200

    Start of XForm input stuff.

---
+++ vodka/tests/test_xinputs.py
@@ -0,0 +1,52 @@
+"""Test for vodka.xinputs."""
+
+from unittest import TestCase
+from xml.etree import ElementTree
+
+from vodka.xinputs import InputInput, Select1Input
+
+
+EXAMPLE1 = """
+<input ref="/data/Name">
+  <label ref="jr:itext('/data/Name:label')"/>
+  <hint ref="jr:itext('/data/Name:hint')"/>
+</input>
+"""
+
+EXAMPLE2 = """
+<select1 ref="/data/Favourite cheese">
+  <label ref="jr:itext('/data/Favourite cheese:label')"/>
+  <hint ref="jr:itext('/data/Favourite cheese:hint')"/>
+  <item>
+    <label ref="jr:itext('/data/Favourite cheese:option0')"/>
+    <value>gouda</value>
+  </item>
+  <item>
+    <label ref="jr:itext('/data/Favourite cheese:option1')"/>
+    <value>cheddar</value>
+  </item>
+</select1>
+"""
+
+
+class TestInputInput(TestCase):
+
+    def test_input(self):
+        input_elem = ElementTree.fromstring(EXAMPLE1)
+        inp = InputInput(input_elem)
+        self.assertEqual("/data/Name", inp.ref)
+        self.assertEqual("jr:itext('/data/Name:label')", inp.label_ref)
+        self.assertEqual("jr:itext('/data/Name:hint')", inp.hint_ref)
+
+
+class TestSelect1Input(TestCase):
+
+    def test_valid(self):
+        input_elem = ElementTree.fromstring(EXAMPLE2)
+        inp = Select1Input(input_elem)
+        self.assertEqual('/data/Favourite cheese', inp.ref)
+        self.assertEqual("jr:itext('/data/Favourite cheese:label')",
+                         inp.label_ref)
+        self.assertEqual("jr:itext('/data/Favourite cheese:hint')",
+                         inp.hint_ref)
+        self.assertEqual(['gouda', 'cheddar'], inp.get_values())
--- vodka/xinputs.py
+++ vodka/xinputs.py
@@ -1,9 +1,57 @@
+# -*- test-case-name: vodka.tests.test_xinputs -*-
+
 """
 XForms input implementations.
 """
+
+
+def get_child(elem, child_tag):
+    for child in elem:
+        if child.tag == child_tag:
+            return child
+    return None
+
+
+def get_child_attr(elem, child_tag, attr):
+    child = get_child(elem, child_tag)
+    if child is not None:
+        return child.get(attr)
+    return None
 
 
 class XFormsInput(object):
     """
     An input mechanism.
     """
+
+    def __init__(self, elem):
+        self.elem = elem
+        self.ref = elem.get('ref')
+        self.label_ref = get_child_attr(elem, 'label', 'ref')
+        self.hint_ref = get_child_attr(elem, 'hint', 'ref')
+
+
+class InputInput(XFormsInput):
+    tag = 'input'
+
+
+class OptionItem(object):
+    def __init__(self, elem):
+        self.elem = elem
+        self.ref = elem.get('ref')
+        self.value = get_child(elem, 'value').text
+        self.label_ref = get_child_attr(elem, 'label', 'ref')
+
+
+class Select1Input(XFormsInput):
+    tag = 'select1'
+
+    def __init__(self, elem):
+        super(Select1Input, self).__init__(elem)
+        self.items = []
+        for child in elem:
+            if child.tag == 'item':
+                self.items.append(OptionItem(child))
+
+    def get_values(self):
+        return [item.value for item in self.items]
