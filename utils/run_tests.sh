e3fe74ce852105fea0a01e8b0c83b3acf8a10492
Simon Cross <hodgestar@gmail.com>
2011-12-21 14:24:20 +0200

    Add build infrastructure.

---
+++ utils/run_tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd $(dirname $0)/..
+
+echo "=== Nuking old .pyc files..."
+find vodka/ -name '*.pyc' -delete
+echo "=== Erasing previous coverage data..."
+coverage erase
+echo "=== Running trial tests..."
+coverage run ./ve/bin/trial --reporter=subunit vodka | tee results.txt | subunit2junitxml > test_results.xml
+subunit2pyunit < results.txt
+rm results.txt
+echo "=== Processing coverage data..."
+coverage xml
+echo "=== Checking for PEP-8 violations..."
+pep8 --repeat vodka > pep8.txt
+echo "=== Done."
